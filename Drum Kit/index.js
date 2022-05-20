551c2ebbc2f62677bbd06d6ff0db3c3909093de1
pNitesh01 <patwanitesh1997@gmail.com>
2022-05-20 13:14:46 +0530

    Add animation to buttons

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -10,6 +10,8 @@ for(var i = 0; i < noOfDrumButtons; i++){
 
         makeSound(buttonInnerHTML);
 
+        buttonAnimation(buttonInnerHTML);
+
     });
 }
 
@@ -17,6 +19,8 @@ for(var i = 0; i < noOfDrumButtons; i++){
 document.addEventListener("keydown", function(event) {
 
     makeSound(event.key);
+
+    buttonAnimation(event.key);
 
 });
 
@@ -55,3 +59,14 @@ function makeSound(key){
     }
 }
 
+function buttonAnimation(currentKey){
+    
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+
+}
