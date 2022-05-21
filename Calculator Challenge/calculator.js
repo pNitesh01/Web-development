e1167fdd6d04e2dde78a383a899ecccca90f13ed
pNitesh01 <patwanitesh1997@gmail.com>
2022-05-21 12:14:19 +0530

    Add BMI calculation functionality

--- Calculator Challenge/calculator.js
+++ Calculator Challenge/calculator.js
@@ -12,7 +12,18 @@ app.post('/', (req, res) => {
     var num1 = parseInt(req.body.num1);
     var num2 = parseInt(req.body.num2);
     var result = num1 + num2;
-    return res.send('The result of calculation is: ' + result)
+    return res.send('The result of calculation is: ' + result);
+});
+
+app.get('/bmicalculator', (req, res) => {
+    return res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
+app.post('/bmicalculator', (req, res) => {
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var result = weight / (height * height);
+    return res.send('Your BMI is: ' + result);
 });
 
 app.listen(3000, () => {
